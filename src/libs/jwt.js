097160a2095c14dddf5e82930aba2a8dff239d56
Sebastian Vazquez <sebastianvazquez928@gmail.com>
2023-10-19 21:08:39 -0600

    uri

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,10 +1,10 @@
-import { TOKEN_SECRET } from "../config.js"
+//import { TOKEN_SECRET } from "../config.js"
 import jwt from "jsonwebtoken"
 
 export function createAccessToken(payload){
 return new Promise ((resolve,reject)=>{
     jwt.sign(
-        payload,TOKEN_SECRET,{
+        payload,'teamolizmucho',{
             expiresIn:"30d"
         },(
           err,token  

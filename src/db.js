84de5de50837d2607825071ec7a16c42c40e7d00
Sebastian Vazquez <sebastianvazquez928@gmail.com>
2023-10-19 19:51:00 -0600

    hi

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-
+import { MONGODB_URI } from "./config.js";
 
 const mongoURI=process.env.MONGODB_URI
 export const connectDB= async ()=>{
     try{
-        await mongoose.connect(mongoURI)
+        await mongoose.connect(MONGODB_URI)
         console.log('BASE DE DATOS CONECTADA:)')
     }catch(error){
 console.log(error)
